869052aa7588519a53476ec0d9e7f4709a727c79
Tom Elrod <tom.elrod@gmail.com>
2018-01-28 01:42:00 -0500

    adding attachment tests

--- README.md
+++ README.md
@@ -141,3 +141,4 @@ To view the API documentation, start the application and then navigate to [http:
 * Issue comments
 * Filtering for issues based on status and priority
 * Finish CRUD (delete and update) for users
+* Delete uploaded files on local filesystem after running attachment tests
--- test/attachment.test.js
+++ test/attachment.test.js
@@ -6,44 +6,107 @@ import Document from '../src/models/document';
 const request = require('supertest')(app);
 const expect = require('chai').expect;
 
-// describe('Attachments', () => {
-//   beforeEach((done) => { //Before each test we empty the database
-//     Issue.remove({}, (err) => {
-//       done();
-//     });
-//   });
-
-  describe('/POST attachment', async () => {
-    it('should create a new image attachment for issue when success', (done) => {
-      let issue = new Issue ({
-        title: "Test Attachment",
-        priority: 1,
-        status: 1,
-        description: "This is a test"
-      });
-      issue.save((error, newIssue) => {
-        const fileName = 'apple.png';
-        const issueId = newIssue.id;
-        request.post('/issue/' + issueId + '/attachment')
-          .attach('attachment', 'test/files/' + fileName)
-          .end((err, res) => {
-            expect(res.status).to.equal(201);
-            expect(res.body.name).to.equal(fileName);
-            expect(res.body.mimetype).to.equal("image/png");
-            expect(res.body).to.have.property("createdAt");
-            expect(res.body).to.have.property("updatedAt");
-            expect(res.body).to.have.property("id");
-            // check that issue now contains attachment id
-            const attachmentId = res.body.id;
-            Issue.findById({_id: issueId}, (err, updatedIssue) => {
-              console.log("updated issue attachment");
-              console.log(updatedIssue);
-              expect(updatedIssue).to.have.property("attachments");
-              let attachments = updatedIssue.attachments;
-              expect(attachments).to.include(attachmentId);
-              done();
-            });
-          })
+describe('Attachments', () => {
+  beforeEach((done) => { //Before each test we empty the database
+    Document.remove({}, (err) => {
+      Issue.remove({}, (err) => {
+        done();
       });
     });
   });
+});
+
+describe('/POST attachment', async () => {
+  it('should create a new image attachment for issue when success', (done) => {
+    let issue = new Issue({
+      title: "Test Attachment",
+      priority: 1,
+      status: 1,
+      description: "This is a test"
+    });
+    issue.save((error, newIssue) => {
+      const fileName = 'apple.png';
+      const issueId = newIssue.id;
+      request.post('/issue/' + issueId + '/attachment')
+        .attach('attachment', 'test/files/' + fileName)
+        .end((err, res) => {
+          expect(res.status).to.equal(201);
+          expect(res.body.name).to.equal(fileName);
+          expect(res.body.mimetype).to.equal("image/png");
+          expect(res.body).to.have.property("createdAt");
+          expect(res.body).to.have.property("updatedAt");
+          expect(res.body).to.have.property("id");
+          // check that issue now contains attachment id
+          const attachmentId = res.body.id;
+          Issue.findById({_id: issueId}, (err, updatedIssue) => {
+            expect(updatedIssue).to.have.property("attachments");
+            let attachments = updatedIssue.attachments;
+            expect(attachments).to.include(attachmentId);
+            done();
+          });
+        })
+    });
+  });
+});
+
+describe('/GET attachment', async () => {
+  it('should stream image attachment for issue when success', (done) => {
+    let issue = new Issue({
+      title: "Test Attachment",
+      priority: 1,
+      status: 1,
+      description: "This is a test"
+    });
+    issue.save((error, newIssue) => {
+      const fileName = 'apple.png';
+      const issueId = newIssue.id;
+      request.post('/issue/' + issueId + '/attachment')
+        .attach('attachment', 'test/files/' + fileName)
+        .end((err, res) => {
+          expect(res.status).to.equal(201);
+          const attachmentId = res.body.id;
+          request.get('/issue/' + issueId + '/attachment/' + attachmentId)
+            .expect('Content-Type', 'image/png')
+            .end((err, res) => {
+              expect(res.status).to.equal(200);
+              done();
+            });
+        })
+    });
+  });
+});
+
+describe('/DELETE attachment', async () => {
+  it('should delete image attachment for issue when success', (done) => {
+    let issue = new Issue({
+      title: "Test Attachment",
+      priority: 1,
+      status: 1,
+      description: "This is a test"
+    });
+    issue.save((error, newIssue) => {
+      const fileName = 'apple.png';
+      const issueId = newIssue.id;
+      request.post('/issue/' + issueId + '/attachment')
+        .attach('attachment', 'test/files/' + fileName)
+        .end((err, res) => {
+          expect(res.status).to.equal(201);
+          const attachmentId = res.body.id;
+          request.delete('/issue/' + issueId + '/attachment/' + attachmentId)
+            .end((err, res) => {
+              expect(res.status).to.equal(204);
+              // now verify document deleted
+              Document.findById({_id: attachmentId}, (err, deletedAttachment) => {
+                expect(deletedAttachment).to.be.null;
+                Issue.findById({_id: issueId}, (err, updatedIssue) => {
+                  expect(updatedIssue).to.have.property("attachments");
+                  let attachments = updatedIssue.attachments;
+                  expect(attachments).to.not.include(attachmentId);
+                  done();
+                });
+              });
+            });
+        })
+    });
+  });
+});
